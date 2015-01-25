208e49be8fadd919e311ea9a4d327c7e240fe850
Kyle Bennett <kyle.2.bennett@plus.net>
2015-01-25 22:45:19 +0000

    Gruffalo v1.0

--- BasicRobots/kylebennett/TheGruffalo.java
+++ BasicRobots/kylebennett/TheGruffalo.java
@@ -1,5 +1,8 @@
 package kylebennett;
 
+import java.awt.Color;
+import java.util.Random;
+
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
@@ -13,43 +16,75 @@ import robocode.WinEvent;
  */
 public class TheGruffalo extends Robot {
 
+	private byte moveDirection = 1;
+	private Random random = new Random();
+	
     @Override
     public void run() {
+    	
+    	setColour();
 
         while (true) {
 
-            ahead(100);
-            turnGunLeft(360);
-
-            turnLeft(180);
+        	if(getTime() % 60 == 0){
+        		turnLeft(45 * moveDirection);
+        	}
+        	
+            ahead(getMoveDistance() * moveDirection);
+            turnGunLeft(360 * moveDirection);
+            
+            moveDirection *= -1;      
         }
     }
 
-    @Override
+    private void setColour() {
+		setColors(Color.BLUE, Color.ORANGE, Color.BLUE, Color.ORANGE, Color.ORANGE);
+	}
+
+	private int getMoveDistance() {
+    	
+		return random.nextInt(200) + 100;
+	}
+
+	@Override
     public void onScannedRobot(ScannedRobotEvent event) {
-
-        fire(2);
-
+    	
+    	if(canFire(event) ){
+    		fire(3);
+    	}
     }
 
-    @Override
-    public void onHitByBullet(HitByBulletEvent event) {
+    private boolean canFire(ScannedRobotEvent event) {
 
+    	if(getGunHeat() == 0){
+    		return true;
+    	}
+		return false;
+	}
+
+	@Override
+    public void onHitByBullet(HitByBulletEvent event) {
+    	turnLeft(60 * moveDirection);
+    	ahead(50 * moveDirection);
+    	turnGunLeft(360 * moveDirection);
     }
 
     @Override
     public void onHitRobot(HitRobotEvent event) {
-
+    	turnLeft(180);
+    	ahead(50 * moveDirection);
+    	turnGunLeft(360 * moveDirection);
     }
 
     @Override
     public void onHitWall(HitWallEvent event) {
-
+    	turnLeft(180);
+    	ahead(50 * moveDirection);
+    	turnGunLeft(360 * moveDirection);
     }
 
     @Override
     public void onWin(WinEvent event) {
 
     }
-
 }
