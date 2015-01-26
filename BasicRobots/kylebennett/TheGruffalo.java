bd60a64b857047b9b44abc1e672d965496f2f862
kylebennett <kylebennett87@gmail.com>
2015-01-26 22:06:54 +0000

    Refinements to TheGruffalo - v1.1

--- BasicRobots/kylebennett/TheGruffalo.java
+++ BasicRobots/kylebennett/TheGruffalo.java
@@ -1,7 +1,8 @@
 package kylebennett;
 
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
 import java.awt.Color;
-import java.util.Random;
 
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
@@ -16,75 +17,104 @@ import robocode.WinEvent;
  */
 public class TheGruffalo extends Robot {
 
-	private byte moveDirection = 1;
-	private Random random = new Random();
-	
+    private byte moveDirection = 1;
+
     @Override
     public void run() {
-    	
-    	setColour();
+
+        // Make it Ginger
+        setColour();
+
+        // Move the gun and radar independently of the body
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
 
         while (true) {
 
-        	if(getTime() % 60 == 0){
-        		turnLeft(45 * moveDirection);
-        	}
-        	
-            ahead(getMoveDistance() * moveDirection);
-            turnGunLeft(360 * moveDirection);
-            
-            moveDirection *= -1;      
+            // switch directions every 30 turns
+            if (getTime() % 30 == 0) {
+                moveDirection *= -1;
+            }
+
+            // Move in a hexagonal pattern
+            ahead(75);
+            turnRadarLeft(360 * moveDirection);
+            turnLeft(60 * moveDirection);
         }
     }
 
-    private void setColour() {
-		setColors(Color.BLUE, Color.ORANGE, Color.BLUE, Color.ORANGE, Color.ORANGE);
-	}
-
-	private int getMoveDistance() {
-    	
-		return random.nextInt(200) + 100;
-	}
-
-	@Override
+    @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-    	
-    	if(canFire(event) ){
-    		fire(3);
-    	}
+
+        // If I can fire...
+        if (canFire(event)) {
+
+            // turn the gun to the scanned robot
+            turnGun(event.getBearing());
+
+            // and fire!
+            fire(2);
+        }
     }
 
     private boolean canFire(ScannedRobotEvent event) {
 
-    	if(getGunHeat() == 0){
-    		return true;
-    	}
-		return false;
-	}
+        // if the gun heat is 0 fire, otherwise don't waste the turn
+        if (getGunHeat() == 0 && getEnergy() > 20) {
+            return true;
+        }
+        return false;
+    }
 
-	@Override
+    @Override
     public void onHitByBullet(HitByBulletEvent event) {
-    	turnLeft(60 * moveDirection);
-    	ahead(50 * moveDirection);
-    	turnGunLeft(360 * moveDirection);
+
+        // Switch directions if I get hit
+        moveDirection *= -1;
+
     }
 
     @Override
     public void onHitRobot(HitRobotEvent event) {
-    	turnLeft(180);
-    	ahead(50 * moveDirection);
-    	turnGunLeft(360 * moveDirection);
+
+        // turn the gun to the other bot
+        turnGun(event.getBearing());
+
+        // point blank shot!
+        fire(3);
+
+        // back up
+        back(20);
+
+        // scan to see if its still there
+        turnRadarLeft(360 * moveDirection);
     }
 
     @Override
     public void onHitWall(HitWallEvent event) {
-    	turnLeft(180);
-    	ahead(50 * moveDirection);
-    	turnGunLeft(360 * moveDirection);
+
+        // Turn away from the wall... I think
+        double turnAmt = normalRelativeAngleDegrees(event.getBearing());
+        turnLeft(turnAmt);
+
+        // move away
+        ahead(100);
+        turnRadarLeft(360 * moveDirection);
+    }
+
+    public void turnGun(double bearing) {
+
+        // turn the gun to point at the other bot
+        double gunTurnAmt = normalRelativeAngleDegrees(bearing + (getHeading() - getGunHeading()));
+        turnGunRight(gunTurnAmt);
+    }
+
+    private void setColour() {
+        setColors(Color.ORANGE, Color.BLUE, Color.ORANGE, Color.BLUE, Color.BLUE);
     }
 
     @Override
     public void onWin(WinEvent event) {
-
+        turnRight(1080);
     }
 }
