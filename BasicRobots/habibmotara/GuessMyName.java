6fa1a30f17a7eca0f0a8a1574252bdab93b9a6fb
hmontara <habib.matora@plus.net>
2015-02-27 10:40:08 +0000

    updated robot

--- BasicRobots/habibmotara/GuessMyName.java
+++ BasicRobots/habibmotara/GuessMyName.java
@@ -1,13 +1,13 @@
 package habibmotara;
 
 import java.awt.AWTException;
+import java.awt.Color;
 
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
-import robocode.WinEvent;
 
 
 /**
@@ -15,7 +15,14 @@ import robocode.WinEvent;
  */
 
 public class GuessMyName extends Robot {
-
+	double previousEnergy = 100;
+	int movementDirection = 1;
+	int gunDirection = 1;
+	private HitByBulletEvent e;
+	
+	public void run() {
+	    turnGunRight(99999);
+	}
 	public GuessMyName() throws AWTException {
 		super();
 		// TODO Auto-generated constructor stub
@@ -24,13 +31,19 @@ public class GuessMyName extends Robot {
 	/**
 	 * run: MyRobot default behavior
 	 */
-	public void run() {
+	
+	public void setColors() {
+		setColors(Color.yellow,Color.blue,Color.red); // body,gun,radar
+	}
+	
+	public void run1() {
 		// Initialization of the robot
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		
+		 	setColors(Color.yellow,Color.blue,Color.red); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
+			// Replace the next 4 lines with any behavior
 			ahead(100);
 			turnGunRight(90);
 			turnLeft(100);
@@ -40,21 +53,47 @@ public class GuessMyName extends Robot {
 
 		@Override
 	    public void onScannedRobot(ScannedRobotEvent event) {
-
-	        fire(2);
-	        ahead(100);
-
+			fire (2);
+			int distance = 0;
+			
+			if(distance>200)
+			{
+			fire(2);
+	        back(100);
+			}
+			if(distance<200)
+	        {
+	           fire(2);
+	        }
+	        else if(distance<500)
+	        {
+	           fire(2);
+	        }
+	        else if(distance<800)
+	        {
+	           fire(2);
+	        }
+	        else
+	        {
+	           ahead(100);
+	        }	
+			e = null;
+			turnRight(e.getBearing()+90-
+			         30*movementDirection);
+			gunDirection = -gunDirection;
+		    turnGunRight(99999*gunDirection);
 	    }
 
-	    @Override
+		@Override
 	    public void onHitByBullet(HitByBulletEvent event) {
 	    	back(10);
+	    	fire(2);
 	    }
 
 	    @Override
 	    public void onHitRobot(HitRobotEvent event) {
 			back(100);
-			fire(1);
+			fire(2);
 
 	    }
 
