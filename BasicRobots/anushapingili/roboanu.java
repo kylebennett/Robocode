9ddc8ce29ec2aa92e5d85b92e489005e7ac0f50b
anusha pingili <anusha.reddypingili@plus.net>
2015-02-27 15:04:58 +0000

    Anushas bot

---
+++ BasicRobots/anushapingili/roboanu.java
@@ -0,0 +1,90 @@
+package anushapingili;
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
+
+/**
+ * This robot extends a basic robot.
+ *
+ * See http://robowiki.net/wiki/Robocode/My_First_Robot for a good beginner tutorial.
+ *
+ * All movement and shooting methods have been defined, you just need to slot them in where you want
+ * them.
+ *
+ * Movement
+ *
+ * - ahead(distance); Move forward, distance in pixels
+ *
+ * - back(distance); move backwards, distance in pixels
+ *
+ * - turnLeft(degrees); / turnRight(degrees); turn your bot to the left or right
+ *
+ * - turnGunLeft(degrees); / turnGunRight(degrees); turn your gun to the left or right
+ *
+ * - turnRadarLeft(degrees); / turnRadarRight(degrees); turn your radar left or right
+ *
+ * Shooting
+ *
+ * - fire(power); fire a bullet
+ *
+ */
+public class Roboanu extends Robot {
+
+    /**
+     * The default behaviour of your bot goes in this method.
+     */
+    @Override
+    public void run() {
+
+        while (true) {
+
+            ahead(1000);
+
+        }
+    }
+
+    /**
+     * Called when your bot scans another bot.
+     */
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+
+    }
+
+    /**
+     * Called when your bot is hit by a bullet.
+     */
+    @Override
+    public void onHitByBullet(HitByBulletEvent event) {
+
+    }
+
+    /**
+     * Called when your bot ccrashes into another bot.
+     */
+    @Override
+    public void onHitRobot(HitRobotEvent event) {
+
+    }
+
+    /**
+     * Called when your bot crashes into a wall.
+     */
+    @Override
+    public void onHitWall(HitWallEvent event) {
+
+    }
+
+    /**
+     * Called when your bot wins... Do a little victory dance.
+     */
+    @Override
+    public void onWin(WinEvent event) {
+
+    }
+
+}
